5e23189ec7ed277675e10958c1cf6c3945b36166
Lucas Nakahara <nakaharalucas2@gmail.com>
2025-03-02 01:06:08 -0300

    primeiro commit

---
+++ index.ts
@@ -0,0 +1,12 @@
+//typescript da quinta série...
+
+function quinta {
+    //
+}
+
+function endurecer() {
+//todo o código do endurecer
+}
+function amolecer () {
+//todo o código do amolecer    
+}
